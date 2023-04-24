2ebbc8bb9e04883e42e5d986a79d857d0a92ec14
achehrynets <andrii.chehrynets@gmail.com>
2023-04-24 21:36:19 +0200

    Implemented MinesweeperException class as generic exception

---
+++ src/main/java/com/acheh/minesweeper/exception/MinesweeperException.java
@@ -0,0 +1,12 @@
+package com.acheh.minesweeper.exception;
+
+/**
+ * This class represents a generic exception for the Minesweeper game.
+ */
+public class MinesweeperException extends RuntimeException {
+
+    public MinesweeperException(String message) {
+        super(message);
+    }
+
+}
